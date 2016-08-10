05525f6049f371a23e091727384ba56c9d58a2ee
Etienne Dauvergne <contact@ekyna.com>
2016-08-10 21:14:45 +0200

    [Resource] Initial commit

---
+++ Dispatcher/ResourceEventDispatcher.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ekyna\Bundle\ResourceBundle\Dispatcher;
+
+use Symfony\Component\EventDispatcher\ContainerAwareEventDispatcher;
+
+/**
+ * Class ResourceEventDispatcher
+ * @package Ekyna\Bundle\ResourceBundle\Dispatcher
+ * @author  Etienne Dauvergne <contact@ekyna.com>
+ */
+class ResourceEventDispatcher extends ContainerAwareEventDispatcher
+{
+
+}
---
+++ EkynaResourceBundle.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Ekyna\Bundle\ResourceBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\EventDispatcher\DependencyInjection\RegisterListenersPass;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class EkynaResourceBundle
+ * @package Ekyna\Bundle\ResourceBundle
+ * @author  Etienne Dauvergne <contact@ekyna.com>
+ */
+class EkynaResourceBundle extends Bundle
+{
+    /**
+     * @inheritDoc
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new RegisterListenersPass(
+            'ekyna_resource.event_dispatcher',
+            'resource.event_listener',
+            'resource.event_subscriber'
+        ));
+    }
+}
